f500d470f4b5a7e35d43fb83506b1fc42d75f249
Sebastian Walz <sivizius@ohai.su>
2020-04-03 03:26:09 +0200

    kinky

--- networking.nix
+++ networking.nix
@@ -2,12 +2,23 @@ let
   this                                  =   import  ./this.nix;
   master                                =   true;
   masters                               =   [ ];
-  slaves                                =   [ ];
+  slaves                                =
+  [
+    "2a0f:4ac0::1"
+    "2a0f:4ac0::4"
+    "2a0f:4ac0::5" 
+  ];
 in
   { ... }:
   {
     networking                          =
     {
+      defaultGateway6                   =
+      {
+        address                         =   "fe80::1";
+        interface                       =   "ens3";
+      };
+
       firewall                          =
       {
         allowedTCPPorts                 =
@@ -19,8 +30,22 @@ in
           53    # dns
         ];
       };
+      
       hostName                          =   this.hostName;
-      interfaces.ens3.useDHCP           =   true;
+
+      interfaces                        =
+      {
+        ens3.useDHCP                    =   true;
+        ens3.ipv6.addresses             =
+        [
+          {
+            address                     =   "2a01:4f9:c010:6bf5::23";
+            prefixLength                =   64;
+          }
+        ];
+      };
+
+      useDHCP                           =   false;
     };
 
     services.bind                       =
@@ -32,7 +57,7 @@ in
       [
         {
           name                          =   this.domain;
-          file                          =   "./zones/${this.domain}";
+          file                          =   "${./zones}/${this.domain}";
           inherit master masters slaves;
         }
       ];
